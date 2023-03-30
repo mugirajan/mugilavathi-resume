28e5fe8951fe64b574b212753b0b0fec52861e27
mugilavathi <mugirajan95@gmail.com>
2023-03-30 11:42:37 +0530

    add my file

--- main.js
+++ main.js
@@ -19,4 +19,51 @@ $(document).ready(function(){
         'linear'
         );
     });
-})
+});
+
+var button=document.getElementById("menuu");
+var div=document.querySelector(".menu-bar");
+var link1=document.querySelector(".link1");
+var link2=document.querySelector(".link2");
+var link3=document.querySelector(".link3");
+var link4=document.querySelector(".link4");
+var link5=document.querySelector(".link5");
+
+
+button.addEventListener("click",()=>{
+    div.style.display="flex";
+});
+
+function clear(){
+   div.style.display="none";
+   
+}
+
+link1.addEventListener("click",()=>{
+    clear()
+});
+
+link2.addEventListener("click",()=>{
+    clear()
+});
+
+link3.addEventListener("click",()=>{
+    clear()
+});
+
+link4.addEventListener("click",()=>{
+    clear()
+});
+
+link5.addEventListener("click",()=>{
+    clear()
+});
+
+
+
+
+
+
+
+
+ 
